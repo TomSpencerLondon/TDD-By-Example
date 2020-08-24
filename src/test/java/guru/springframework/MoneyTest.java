0f731e2c60c882d84d8d63834a118fbc035b57fb
Tom <thomas.spencer@codurance.com>
2020-08-24 15:02:52 +0100

    Add passing multiplication test with new object return

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -4,12 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+//$5 + 10 CHF = $10 (with rate of 2:1)
+//    • $5 * 2 = $10
+//    • Make “amount” private
+//• Dollar side effects?
+//    • Money Rounding?
+
 public class MoneyTest {
 
   @Test
   void multiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+    Dollar product = five.times(2);
+    assertEquals(10, product.amount);
+    product = five.times(3);
+    assertEquals(15, product.amount);
   }
 }
